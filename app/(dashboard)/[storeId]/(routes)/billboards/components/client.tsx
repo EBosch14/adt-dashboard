e012b528c6a5e17454e9e3a02557626db498593a
EBosch14 <enzo.jbosch@gmail.com>
2023-09-20 20:51:05 -0300

    feat(/*): :sparkles: add api routes publics and privates

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -31,6 +32,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="Llamadas a la API para los paneles" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
